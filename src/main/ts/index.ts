55895e78b36054c324b5d7589f7808f8b9286cc2
navima <draco.109@gmail.com>
2024-03-07 22:30:28 +0100

    Update ingores and add intex

---
+++ src/main/ts/index.ts
@@ -0,0 +1,8 @@
+export * from '../../../target/ts/DscriptLexer';
+export { default as DscriptLexer } from '../../../target/ts/DscriptLexer';
+export * from '../../../target/ts/DscriptParser';
+export { default as DscriptParser } from '../../../target/ts/DscriptParser';
+export * from '../../../target/ts/DscriptParserVisitor';
+export { default as DscriptParserVisitor } from '../../../target/ts/DscriptParserVisitor';
+export * from '../../../target/ts/DscriptParserListener';
+export { default as DscriptParserListener } from '../../../target/ts/DscriptParserListener';
