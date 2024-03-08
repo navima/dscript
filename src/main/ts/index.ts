1e49d8a81641e815e83960a7dcbacf317abdf9e7
navima <draco.109@gmail.com>
2024-03-08 21:59:02 +0100

    fuck this shit

--- src/main/ts/index.ts
+++ src/main/ts/index.ts
@@ -1,5 +1,5 @@
-export { default as DscriptLexer } from './generated/DscriptLexer';
-export { default as DscriptParser } from './generated/DscriptParser';
-export * from './generated/DscriptParser';
-export { default as DscriptParserVisitor } from './generated/DscriptParserVisitor';
-export { default as DscriptParserListener } from './generated/DscriptParserListener';
+export { default as DscriptLexer } from './DscriptLexer';
+export { default as DscriptParser } from './DscriptParser';
+export * from './DscriptParser';
+export { default as DscriptParserVisitor } from './DscriptParserVisitor';
+export { default as DscriptParserListener } from './DscriptParserListener';
