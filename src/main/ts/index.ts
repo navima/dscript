4365c22b753c484af6966488bfbf834bcca32b11
navima <draco.109@gmail.com>
2024-03-08 21:13:36 +0100

    Add compilation step

--- src/main/ts/index.ts
+++ src/main/ts/index.ts
@@ -1,8 +1,5 @@
-export * from '../../../target/ts/DscriptLexer';
-export { default as DscriptLexer } from '../../../target/ts/DscriptLexer';
-export * from '../../../target/ts/DscriptParser';
-export { default as DscriptParser } from '../../../target/ts/DscriptParser';
-export * from '../../../target/ts/DscriptParserVisitor';
-export { default as DscriptParserVisitor } from '../../../target/ts/DscriptParserVisitor';
-export * from '../../../target/ts/DscriptParserListener';
-export { default as DscriptParserListener } from '../../../target/ts/DscriptParserListener';
+export { default as DscriptLexer } from './generated/DscriptLexer';
+export { default as DscriptParser } from './generated/DscriptParser';
+export * from './generated/DscriptParser';
+export { default as DscriptParserVisitor } from './generated/DscriptParserVisitor';
+export { default as DscriptParserListener } from './generated/DscriptParserListener';
